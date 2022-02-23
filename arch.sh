8a0ffeb9ef9e9594717f34b3acd0afe2c45fe384
Igor Gnatyuk <igorgn@wix.com>
2022-02-23 19:14:44 +0200

    test

--- arch.sh
+++ arch.sh
@@ -5,4 +5,4 @@ touch coverage/1.lcov
 touch coverage/2.lcov
 touch coverage/3.lcov
 
-tar czf coverage_$BUILDKITE_STEP_ID.tar.gz
+tar czf coverage_$BUILDKITE_STEP_ID.tar.gz coverage/*.lcov
--- exec.sh
+++ exec.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 buildkite-agent artifact download "**/coverage*.tar.gz" . --build $BUILDKITE_BUILD_ID
+find . -name "coverage*.tar.gz" -exec tar -xf {} \;
 ls -la
