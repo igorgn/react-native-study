a92f3aa027996bfdc520a6d56eb902093e899d89
Igor Gnatyuk <igorgn@wix.com>
2023-02-19 17:11:33 +0200

    clone

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 sed -i '' 's|^registry.*|registry=http://ci-cache.internal:8081/repository/npm-wixpress/|' ~/.npmrc
 nvm install
-npm install
+npm install npm@8
+npm install --legacy-peer-deps
 
 $COMM
