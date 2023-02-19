3fdd6b4fa29212a44a9ad7ae679428d96bf1a932
Igor Gnatyuk <igorgn@wix.com>
2023-02-19 16:17:20 +0200

    clone

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ FN=$(echo $REPO | awk  -F / '{ print $2 }' | cut -d . -f 1)
 echo "Cloning into $FN"
 git clone $REPO
 cd $FN
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 nvm install
 npm install
 
