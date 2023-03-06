b5392fa8e62dec62ad65373305b959c0df8df63a
Igor Gnatyuk <igorgn@wix.com>
2023-03-06 13:14:48 +0200

    wip

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -e
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+sed -i '' 's|^registry.*|registry=http://ci-cache.internal:8081/repository/npm-wixpress/|' ~/.npmrc
+nvm install 14
+
 echo "REPO is $REPO and command is $COMM"
 FN=$(echo $REPO | awk  -F / '{ print $2 }' | cut -d . -f 1)
 echo "Cloning into $FN"
 git clone $REPO && cd $FN
 git checkout $REV
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-sed -i '' 's|^registry.*|registry=http://ci-cache.internal:8081/repository/npm-wixpress/|' ~/.npmrc
-nvm install
 npm install npm@8
 npm install --legacy-peer-deps
 
