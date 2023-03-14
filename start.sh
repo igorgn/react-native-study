ef48258c158aaf16578e4917ccd3ab69077f2a8a
Igor Gnatyuk <igorgn@wix.com>
2023-03-14 12:09:54 +0200

    allure generate

--- start.sh
+++ start.sh
@@ -9,7 +9,8 @@ sed -i '' 's|^registry.*|registry=http://ci-cache.internal:8081/repository/npm-w
 echo "REPO is $REPO and command is $COMM"
 FN=$(echo $REPO | awk  -F / '{ print $2 }' | cut -d . -f 1)
 echo "Cloning into $FN"
-git clone $REPO && cd $FN
+git clone $REPO 
+cd $FN
 git checkout $REV
 
 npm install npm@8
@@ -18,3 +19,16 @@ export PATH=$(pwd)/node_modules/.bin:$PATH
 
 echo $COMM
 bash -c "$COMM"
+bash generate_allure_report.sh
+
+cd ..
+
+generate_allure_report() {
+    rm -rf "artifacts/allure-results" && mkdir -p artifacts
+
+    find artifacts -type d -name allure-results -exec cp -r {} artifacts \;
+
+    if [ -d "artifacts/allure-results" ];then
+        cd artifacts && allure generate --clean || echo 'allure is not installed..'
+fi
+}
