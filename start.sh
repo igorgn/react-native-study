3e3a1981a654f9a5619f0e43d402a12534ee4dc0
Igor Gnatyuk <igorgn@wix.com>
2023-03-19 15:33:16 +0200

    return allure download link

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo $BUILDKITE_AGENT_ACCESS_TOKEN > ~/token
+# echo $BUILDKITE_AGENT_ACCESS_TOKEN > ~/token
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install 14
@@ -30,5 +30,10 @@ find $FN -type d -name allure-results -exec cp -r {} artifacts \;
 if [ -d "artifacts/allure-results" ];then
     cd artifacts && allure generate --clean || echo 'allure is not installed..'
     tar -zcf allure-$BUILDKITE_PIPELINE_SLUG-$BUILDKITE_BUILD_NUMBER.tgz allure-report
-fi
 
+    #Upload artifact
+    ARTIFACT_ID=$(buildkite-agent artifact upload --job $BUILDKITE_JOB_ID --agent-access-token $BUILDKITE_AGENT_ACCESS_TOKEN allure-$BUILDKITE_PIPELINE_SLUG-$BUILDKITE_BUILD_NUMBER.tgz 2>&1 | grep -o "Uploading artifact.*" | cut -d' ' -f3)
+    buildkite-agent meta-data set "allure_report_download_link" "https://buildkite.com/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_NUMBER/jobs/$BUILDKITE_JOB_ID/artifacts/$ARTIFACT_ID"
+
+    echo "https://buildkite.com/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_NUMBER/jobs/$BUILDKITE_JOB_ID/artifacts/$ARTIFACT_ID"
+fi
